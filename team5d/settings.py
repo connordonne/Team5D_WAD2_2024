4d95f5dcbe81a13aefc235db0eca9a2c5c5f051a
“Erik <2911240m@student.gla.ac.uk>
2024-02-27 20:02:43 +0000

    issue with static files

--- team5d/settings.py
+++ team5d/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # setting up the static file directory
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR,]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 

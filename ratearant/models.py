b5c42a7fcda094803022932f54a98ad5ffd72749
“Erik <2911240m@student.gla.ac.uk>
2024-03-02 13:49:20 +0000

    Added 2 tables in model.py, tested DB locally, with local admin setup, seems ok. pushing to test on python anywhere

--- ratearant/models.py
+++ ratearant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+# Table for the cuisine type, PK is auto generated.
+class Cuisine(models.Model):
+    cuisineId = models.AutoField(primary_key=True)
+    cuisineName = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.cuisineName
+
+# Table for the restaurant, PK is auto generated.
+class restaurant(models.Model):
+    restaurantId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, unique=True)
+    address = models.CharField(max_length=256)
+    phone = models.CharField(max_length=16)
+    website = models.URLField()
+    openingTime = models.TimeField()
+    priceRange = models.CharField(max_length=16)
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

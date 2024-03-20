b7a296e8e4e7496da30d12b2ab912b4f8774276f
Ibrahim Asghar <2617847a@student.gla.ac.uk>
2024-03-20 11:33:54 +0000

    implemented map

---
+++ static/ratearant/scripts/map.js
@@ -0,0 +1,24 @@
+function loadMapScenario() {
+    var searchQuery = document.getElementById('restaurantAddress').innerHTML;                    
+    var map = new Microsoft.Maps.Map(document.getElementById('map'), {
+        /* No need to set credentials if already passed in URL */
+        center: new Microsoft.Maps.Location(55.860916, -4.251433),
+        zoom: 19
+    });
+
+    Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+        var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+        var requestOptions = {
+            bounds: map.getBounds(),
+            where: searchQuery,
+            callback: function (answer, userData) {
+                map.setView({ bounds: answer.results[0].bestView });
+                var pin = new Microsoft.Maps.Pushpin(answer.results[0].location, {
+                    color: 'red'
+                });
+                map.entities.push(pin);
+            }
+        };
+        searchManager.geocode(requestOptions);
+    });
+}

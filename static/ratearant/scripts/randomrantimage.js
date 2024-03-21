94dd4df4b3d5357b5386e9f376c81e747767eff3
Ibrahim Asghar <2617847a@student.gla.ac.uk>
2024-03-21 14:12:40 +0000

    Cycle random images

--- static/ratearant/scripts/randomrantimage.js
+++ static/ratearant/scripts/randomrantimage.js
@@ -1,11 +1,13 @@
 $(document).ready(function (){
-    // store the path to the image
-    var pathToImage = staticUrl + "rateArant_";
-    //get a number between 1-19
-    var nums=19;
-    var random=Math.floor(Math.random()*nums)+1;
-    var image=pathToImage+random+".jpg";
-    console.log(image);
-    $('#restaurantpic').attr('src',image);
+    const cardImages = document.querySelectorAll('[attrrestaurantpic]');
+    cardImages.forEach(cardImage => {
+        var pathToImage = staticUrl + "rateArant_";
+        //get a number between 1-19
+        var nums=19;
+        var random=Math.floor(Math.random()*nums)+1;
+        var image=pathToImage+random+".jpg";
+        console.log(image);
+        cardImage.src = image;
+    });
 });
 

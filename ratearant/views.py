7b40bf69fa9918628b4e31e5d473608016938fa5
Qikai Zhou <2616452q@student.gla.ac.uk>
2024-03-21 09:26:15 +0000

    Code optimization, feature supplementation, addition of comments

--- ratearant/views.py
+++ ratearant/views.py
@@ -1,52 +1,49 @@
 # User login and logout
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.urls import reverse
-
 from ratearant.forms import RestaurantForm, UserForm, ReviewForm, ChangeUserForm
-from ratearant.models import Restaurant, Cuisine, Review, Comment
-from django.contrib.auth.models import User
+from ratearant.models import Restaurant, Cuisine, Review
 from django.contrib import messages
-from django.contrib.auth import update_session_auth_hash
-from django.http import HttpResponseForbidden
 from decimal import Decimal
 
-
-# Create your views here.
-
-
+# home page
 def home(request):
+    # getting the top rated and favourite restaurants
     restaurant_list = Restaurant.objects.order_by('-average_rating')[:5]
     fave_restaurant_list = Restaurant.objects.order_by('-number_of_reviews')[:5]
 
     context_dict = {'top_message': "Top Rated Restaurants",
                     'fave_message': "Favourite Restaurants",
-                    'restaurants': restaurant_list, 'fave_restaurants': fave_restaurant_list,
-                    'range': range(1, 6),
-                    'isHomePage': True,
+                    'restaurants': restaurant_list, 
+                    'fave_restaurants': fave_restaurant_list,
+                    'range': range(1, 6), # number of stars to display
+                    'isHomePage': True, # to display the home page nav bar
                     }
     return render(request, 'ratearant/home.html', context=context_dict)
 
-
+# about page
 def about(request):
     context_dict = {'message': 'Placeholder for the Team 5D Rate-A-Rant About page',
-                    'isAboutPage': True}
+                    'isAboutPage': True # to display the about page nav bar
+                    }
     return render(request, 'ratearant/about.html', context=context_dict)
 
-
+# food styles page
 def food_styles(request):
+    # getting the cuisines and restaurants by order
     restaurants_list = Restaurant.objects.order_by("-cuisine")[:]
     cuisines_list = Cuisine.objects.order_by("-cuisineName")[:]
 
     context_dict = {
         "cuisines": cuisines_list,
         "restaurants": restaurants_list,
-        'range': range(1, 6)
+        'range': range(1, 6) # number of stars to display
     }
     return render(request, 'ratearant/food_styles.html', context=context_dict)
 
-
+# categories page
 def categories(request, cuisineName):
     context_dict = {}
     try:
@@ -56,9 +53,10 @@ def categories(request, cuisineName):
 
         context_dict = {
             "cuisine": cuisine,
-            "restaurants": restaurants
+            "restaurants": restaurants,
+            'range': range(1, 6) # number of stars to display
         }
-
+        # If cuisine does not exist, return an empty list
     except Cuisine.DoesNotExist:
         context_dict = {
             "cuisine": None,
@@ -66,7 +64,7 @@ def categories(request, cuisineName):
         }
     return render(request, 'ratearant/categories.html', context=context_dict)
 
-
+# restaurant page
 def show_restaurant(request, restaurant_name_slug):
     context_dict = {}
 
@@ -75,7 +73,8 @@ def show_restaurant(request, restaurant_name_slug):
         restaurant = Restaurant.objects.get(slug=restaurant_name_slug)
         # getting the reviews associated with the restaurant
         reviews = Review.objects.filter(restaurant=restaurant)
-        context_dict = {'restaurant': restaurant, 'name': restaurant.name,
+        context_dict = {'restaurant': restaurant, 
+                        'name': restaurant.name,
                         'address': restaurant.address,
                         'phone': restaurant.phone,
                         'website': restaurant.website,
@@ -89,8 +88,7 @@ def show_restaurant(request, restaurant_name_slug):
                 context_dict['reviewed'] = True
         else:
             context_dict['reviewed'] = False
-
-
+        # If restaurant does not exist, return an empty list
     except Restaurant.DoesNotExist:
         context_dict['restaurant'] = None
         context_dict['name'] = None
@@ -105,20 +103,22 @@ def show_restaurant(request, restaurant_name_slug):
     return render(request, 'ratearant/restaurant.html', context=context_dict)
 
 
-# User login
+# User login view
 def login_view(request):
     error_message = None
 
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        # Authenticate the user
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             if user.is_active:
                 login(request, user)
                 return redirect(reverse('ratearant:home'))
+        else:
+            error_message = 'Invalid username or password'
 
     context = {
         'error_message': error_message
@@ -127,21 +127,21 @@ def login_view(request):
     return render(request, 'ratearant/login.html', context)
 
 
-# User logout
+# User logout view
 @login_required
 def user_logout(request):
     logout(request)
     return redirect('ratearant:home')
 
-
+# User registration page
 def register(request):
     registered = False
 
-
     if request.method == 'POST':
         user_form = UserForm(request.POST)
-
+        # Check if the form is valid
         if user_form.is_valid():
+            # Save the user
             user = user_form.save(commit=False)
             user.set_password(user_form.cleaned_data['password'])
             user.email = user_form.cleaned_data['email']
@@ -159,8 +159,9 @@ def register(request):
                   context={'user_form': user_form,
                            'registered': registered})
 
-
+# trending page
 def trending(request):
+    # getting the top rated and favourite restaurants
     restaurant_list = Restaurant.objects.order_by('-average_rating')[:5]
     fave_restaurant_list = Restaurant.objects.order_by('-number_of_reviews')
 
@@ -168,46 +169,58 @@ def trending(request):
                     'fave_message': "Favourite Restaurants",
                     'restaurants': restaurant_list,
                     'fave_restaurants': fave_restaurant_list,
-                    'range': range(1, 6)
+                    'range': range(1, 6) # number of stars to display
                     }
     return render(request, 'ratearant/trending.html', context=context_dict)
 
-
+# add review page
 @login_required
 def add_review(request, restaurant_name_slug):
     restaurant = Restaurant.objects.get(slug=restaurant_name_slug)
-
+    # Check if the user has already reviewed the restaurant
     if Review.objects.filter(user=request.user, restaurant_id=restaurant.restaurantId).exists():
         return render(request, 'ratearant/add_review.html', {'reviewed': True})
-
+    # If the user has not reviewed the restaurant, proceed to add a review
     if request.method == 'POST':
         form = ReviewForm(request.POST)
+        # Check if the form is valid
         if form.is_valid():
+            # Save the review
             review = form.save(commit=False)
             review.user = request.user
             review.restaurant = restaurant
             review.foodRating = request.POST.get('foodRating')
             review.serviceRating = request.POST.get('serviceRating')
             review.overallRating = request.POST.get('overallRating')
-            review.averageScore = (int(review.foodRating) + int(review.serviceRating) + int(review.overallRating)) / 3
+            # Calculate the average score
+            review.averageScore = (
+                int(review.foodRating) + 
+                int(review.serviceRating) + 
+                int(review.overallRating)
+                ) / 3
 
             review.save()
 
-            restaurant.average_rating = (restaurant.average_rating*restaurant.number_of_reviews + Decimal(review.averageScore))/ (restaurant.number_of_reviews + 1)
+            # Update the restaurant's average rating and number of reviews
+            restaurant.average_rating = (restaurant.average_rating*restaurant.number_of_reviews + 
+                                         Decimal(review.averageScore))/ (restaurant.number_of_reviews + 1)
             restaurant.number_of_reviews = restaurant.number_of_reviews + 1
             restaurant.save()
             return redirect('ratearant:show_restaurant', restaurant_name_slug=restaurant.slug)
     else:
         form = ReviewForm()
 
+    # If the request method is not POST, display the form
     context = {
         'form': form,
         'restaurant': restaurant
     }
     return render(request, 'ratearant/add_review.html', context)
 
+# edit profile page
 @login_required
 def edit_profile(request):
+    # Initialize the form with the current user instance
     if request.method == 'POST':
         # Initialize the form with POST data and the current user instance
         user_form = ChangeUserForm(request.POST, instance=request.user)
@@ -234,41 +247,65 @@ def edit_profile(request):
 
     return render(request, 'ratearant/edit_profile.html', {'user_form': user_form})
 
+# my comments page
 @login_required
 def my_comments(request):
+    # getting the user's comments
     comments = Review.objects.all()
     reviews = []
+    # Filtering the comments to get only the user's comments
     for eachComment in comments:
         if eachComment.user == request.user:
+            # Adding the comments to the list
             reviews.append(eachComment)
     return render(request, 'ratearant/my_comments.html', {'comments': reviews})
 
+# delete comment page
 @login_required
 def delete_comment(request, reviewId):
+    # getting the user's comments
     comments = Review.objects.all()
+    # Filtering the comments to get only the user's comments
     for eachComments in comments:
+        # if eachComments.user == request.user:
         if eachComments.reviewId == reviewId and eachComments.user == request.user:
+            # updating the restaurant's average rating and number of reviews
             restaurant = eachComments.restaurant
             restaurant.average_rating = (restaurant.average_rating*restaurant.number_of_reviews-eachComments.overallRating)/(restaurant.number_of_reviews-1)
             restaurant.number_of_reviews = restaurant.number_of_reviews -1
             restaurant.save()
+            # deleting the comment
             eachComments.delete()
             break
     return redirect('ratearant:my_comments')  
 
+# add restaurant page
 @login_required
 def add_restaurant(request):
+    # Check if the user is an admin
     if request.method == 'POST':
+        # Initialize the form with POST data
         form = RestaurantForm(request.POST)
+        # Check if the form is valid
         if form.is_valid():
+            # Save the restaurant
+            review = form.save(commit=False)
+            review.user = request.user
+            review.name = request.POST.get('name')
+            review.address = request.POST.get('address')
+            review.website = request.POST.get('website')
+            review.cuisine = Cuisine.objects.get(cuisineId=request.POST.get('cuisine'))
+            review.save()
             request.session['form_data'] = request.POST
             return redirect('ratearant:thank_you')
     else:
         form = RestaurantForm()
     return render(request, 'ratearant/add_restaurant.html', {'form': form})
 
+# thank you page
 @login_required
 def thank_you(request):
+    # Check if the user is an admin
     form_data = request.session.get('form_data')
     if form_data:
         del request.session['form_data']
